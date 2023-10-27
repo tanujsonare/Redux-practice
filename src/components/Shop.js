ff182452fd1ba0f4264107e4cade533ebeeaec33
tanujsonare <tanujsonare1910@gmail.com>
2023-10-27 13:19:20 +0530

    Created a basic Ecommerce project to practice redux

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='mt-5'>
+        <h3>Buy Iphone at 10 Rs</h3>
+      <button className="btn btn-primary mx-3">-</button>
+      Add to cart
+      <button className="btn btn-primary mx-3">+</button>
+    </div>
+  )
+}
+
+export default Shop
