81a31b7ed6ee9831e15d8b07d2a93f3701a83c15
tanujsonare <tanujsonare1910@gmail.com>
2023-10-31 12:58:10 +0530

    Show updated amount using bind action creators

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,12 +5,19 @@ import { actionCreators } from '../state'
 
 const Shop = () => {
   const dispatch = useDispatch();
+  const {withdrawalMoney, depositeMoney} = bindActionCreators(actionCreators, dispatch)
   return (
     <div className='mt-5'>
         <h3>Deposite/withdrawal Money</h3>
-      <button className="btn btn-primary mx-3" onClick={()=>{dispatch(actionCreators.withdrawalMoney(100))}}>-</button>
+      {/* Without using bindActionCreators */}
+      {/* <button className="btn btn-primary mx-3" onClick={()=>{dispatch(actionCreators.withdrawalMoney(100))}}>-</button>
       Update balance
-      <button className="btn btn-primary mx-3" onClick={()=>{dispatch(actionCreators.depositeMoney(100))}}>+</button>
+      <button className="btn btn-primary mx-3" onClick={()=>{dispatch(actionCreators.depositeMoney(100))}}>+</button> */}
+
+      {/* with the use of bindActionCreators */}
+      <button className="btn btn-primary mx-3" onClick={()=>{withdrawalMoney(100)}}>-</button>
+      Update balance
+      <button className="btn btn-primary mx-3" onClick={()=>{depositeMoney(100)}}>+</button>
     </div>
   )
 }
